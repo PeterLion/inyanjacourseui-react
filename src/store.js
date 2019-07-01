97a16b14b404ab73b1ba9a94b41786a213da89db
Pierre Celestin NSHIMYUMUREMYI <nshimiyepeter@gmail.com>
2019-06-30 21:28:29 -0600

    adjusted store

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from "redux"
 import thunk from "redux-thunk"
 import rootReducer from "./reducers"
 
-const initialState = {};
+const initialState = {}
 const middleware = [thunk]
 
 let store
@@ -10,11 +10,12 @@ let store
 const ReactReduxDevTools =
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-if (window.navigator.userAgent.includes("Chrome") && ReactReduxDevTools) {
-    store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware),
-        ReactReduxDevTools))
+if (window.navigator.userAgent.includes("chrome") && ReactReduxDevTools) {
+    store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware), ReactReduxDevTools))
 } else {
     store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)))
 }
 
-export default store
+
+export default store;
+
