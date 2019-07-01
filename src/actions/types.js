1044eb1ccf0e4dd08e6b16b29afbd3753082790e
Pierre Celestin NSHIMYUMUREMYI <nshimiyepeter@gmail.com>
2019-07-01 16:31:58 -0600

    I have corrected action types initialisation

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,4 @@
 
-export const GET_PROGRAMS = GET_PROGRAMS;
-export const GET_TOPICS = GET_TOPICS;
-export const GET_COURSES = GET_COURSES;
+export const GET_PROGRAMS = "GET_PROGRAMS";
+export const GET_TOPICS = "GET_TOPICS";
+export const GET_COURSES = "GET_COURSES";
