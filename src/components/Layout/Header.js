8641bf3555a916c5b5196dc2a289e07e7a14ea18
Pierre Celestin NSHIMYUMUREMYI <nshimiyepeter@gmail.com>
2019-07-06 20:50:33 -0600

    Adjusted navbar theme

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -32,7 +32,7 @@ class Header extends Component {
         }
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-inverse bg-light">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
