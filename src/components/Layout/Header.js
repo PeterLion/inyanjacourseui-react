f2aac99dae167a6ed864145a7867555f11aabd73
Pierre Celestin NSHIMYUMUREMYI <nshimiyepeter@gmail.com>
2019-07-06 20:35:00 -0600

    I have finished the about page

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -32,21 +32,21 @@ class Header extends Component {
         }
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className="navbar navbar-expand-lg navbar-inverse bg-light">
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <a className="navbar-brand" href="#">Hidden brand</a>
+                        <Link className="navbar-brand" to="/">Inyanja Space</Link>
                         <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                             <li className="nav-item active">
-                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                                <Link className="nav-link" to="/">InyaLearn <span className="sr-only">(current)</span></Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
+                                <Link className="nav-link" to="/">InyaServe</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                                <Link to="/" className="nav-link">InyaRead</Link>
                             </li>
                         </ul>
                         <form className="form-inline my-2 my-lg-0 mr-auto">
